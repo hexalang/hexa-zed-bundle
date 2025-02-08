e97eee1b522cab5c1160dd581b6cb7eca6e3c2f8
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2025-02-08 12:40:19 +0200

    Update hexa.rs

--- src/hexa.rs
+++ src/hexa.rs
@@ -1,6 +1,5 @@
 // Based on https://github.com/gavr123456789/zed-niva
 
-// use std::path::Path;
 use zed::LanguageServerId;
 use zed_extension_api::{self as zed, Result};
 
@@ -16,19 +15,12 @@ impl zed::Extension for HexaExtension {
         _language_server_id: &LanguageServerId,
         worktree: &zed::Worktree,
     ) -> Result<zed::Command> {
-        // let hexa_path = Path::new("/home/gavr/.hexa/hexa/bin/hexa");
-        // if !hexa_path.exists() {
-        //     eprintln!("Error: hexa language server binary not found");
-        //     std::process::exit(1);
-        // }
-        // let x = hexa_path.to_str().unwrap_or("").to_string()
-
         let path = worktree
-            .which("hexals")
+            .which("hexa")
             .ok_or_else(|| "hexa must be installed and available on your $PATH".to_string())?;
         Ok(zed::Command {
             command: path,
-            args: vec![],
+            args: vec!["lsp".to_string()],
             env: Default::default(),
         })
     }
